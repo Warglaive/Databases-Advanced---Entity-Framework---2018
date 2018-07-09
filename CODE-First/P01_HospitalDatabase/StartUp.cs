baa228ff101c969f500c0393f314992917ef46e9
warglaivee <warglaivegm@gmail.com>
2018-07-09 19:43:05 +0300

    up

--- CODE-First/P01_HospitalDatabase/StartUp.cs
+++ CODE-First/P01_HospitalDatabase/StartUp.cs
@@ -1,10 +1,17 @@
-﻿namespace P01_HospitalDatabase
+﻿using System;
+using P01_HospitalDatabase.Data;
+using P01_HospitalDatabase.Initializer;
+
+namespace P01_HospitalDatabase
 {
     public class StartUp
     {
         public static void Main()
         {
-
+            using (var hospitalContext = new HospitalContext())
+            {
+                DatabaseInitializer.InitialSeed(hospitalContext);
+            }
         }
     }
 }
