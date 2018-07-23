4ed2607daa2d79cc31edc768f960da21834b8491
warglaivee <warglaivegm@gmail.com>
2018-07-23 18:20:05 +0300

    done

---
+++ AUTO MAPPING OBJECTS/01.Employees Mapping/Core/Commands/ExitCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using Banicharnica.App.Core.Contracts;
+
+namespace Banicharnica.App.Core.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            for (int i = 5; i > 0; i--)
+            {
+                Console.WriteLine($"Program closing in {i} seconds");
+                Thread.Sleep(1000);
+                if (i == 1)
+                {
+                    Console.WriteLine("Bye!");
+                }
+            }
+            Environment.Exit(0);
+            return null;
+        }
+    }
+}
