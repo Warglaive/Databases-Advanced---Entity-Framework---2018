baa604de379a41b6afb8b7560241ba8304ec1c35
warglaivee <warglaivegm@gmail.com>
2018-07-27 17:05:39 +0300

    up

--- PhotoShare Good Practices Project/PhotoShare.Client/Core/Commands/AddFriendCommand.cs
+++ PhotoShare Good Practices Project/PhotoShare.Client/Core/Commands/AddFriendCommand.cs
@@ -1,4 +1,7 @@
-﻿using PhotoShare.Services.Contracts;
+﻿using System.Linq;
+using PhotoShare.Client.Core.Dtos;
+using PhotoShare.Models;
+using PhotoShare.Services.Contracts;
 
 namespace PhotoShare.Client.Core.Commands
 {
@@ -21,11 +24,35 @@ namespace PhotoShare.Client.Core.Commands
             var userExists = this.userService.Exists(username);
             var friendExistrs = this.userService.Exists(friendUsername);
 
-            if (!userExists || !friendExistrs)
+            if (!userExists)
             {
-                throw new ArgumentException($"{}");
+                throw new ArgumentException($"{username} not found!");
+            }
+            if (!friendExistrs)
+            {
+                throw new ArgumentException($"{friendUsername} not found!");
             }
 
+            var user = this.userService.ByUsername<UserFriendsDto>(username);
+            var friend = this.userService.ByUsername<UserFriendsDto>(friendUsername);
+
+            var isSentRequestFromFriend = friend.Friends.Any(n => n.Username == user.Username);
+            var isSentRequestFromUser = user.Friends.Any(n => n.Username == friend.Username);
+
+            if (isSentRequestFromFriend && isSentRequestFromUser)
+            {
+                throw new ArgumentException($"{friend.Username} is already a friend to {user.Username}");
+            }
+            else if (isSentRequestFromUser && !isSentRequestFromFriend)
+            {
+                throw new InvalidOperationException("Request is already send!");
+            }
+            else if (isSentRequestFromFriend && !isSentRequestFromUser)
+            {
+                throw new InvalidOperationException("Request is already send!");
+            }
+            this.userService.AddFriend(user.Id, friend.Id);
+            return $"Friend {friend} added to {user}";
         }
     }
 }
--- PhotoShare Good Practices Project/PhotoShare.Client/Core/Dtos/UserFriendsDto.cs
+++ PhotoShare Good Practices Project/PhotoShare.Client/Core/Dtos/UserFriendsDto.cs
@@ -5,6 +5,7 @@
 
     public class UserFriendsDto
     {
+        public int Id { get; set; }
         public string Username { get; set; }
 
         public ICollection<FriendDto> Friends { get; set; }
