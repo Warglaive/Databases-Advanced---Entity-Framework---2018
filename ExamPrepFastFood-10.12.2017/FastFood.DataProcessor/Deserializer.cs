09813992a28327074bb991e07255aaeaa4d88f25
warglaivee <warglaivegm@gmail.com>
2018-08-09 21:45:52 +0300

    position creation fixed

--- ExamPrepFastFood-10.12.2017/FastFood.DataProcessor/Deserializer.cs
+++ ExamPrepFastFood-10.12.2017/FastFood.DataProcessor/Deserializer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 using AutoMapper;
 using FastFood.Data;
@@ -23,19 +24,41 @@ namespace FastFood.DataProcessor
             var employees = new List<Employee>();
             foreach (var employeeDto in deserializedEmployees)
             {
-                IsEmployeeUnique(employeeDto, deserializedEmployees);
-                if (!IsValid(employeeDto))
+                if (!IsEmployeeUnique(employeeDto, deserializedEmployees) || !IsValid(employeeDto))
                 {
                     sb.AppendLine(FailureMessage);
                     continue;
                 }
+
                 var employee = Mapper.Map<Employee>(employeeDto);
+
+                var position = GetPosition(context, employeeDto.Position);
+
+                employee.Position = position;
+
                 employees.Add(employee);
                 sb.AppendLine(string.Format(SuccessMessage, employee.Name));
             }
+            
             context.Employees.AddRange(employees);
             context.SaveChanges();
             return sb.ToString().Trim();
+        }
+
+        private static Position GetPosition(FastFoodDbContext context, string positionName)
+        {
+            var position = context.Positions.FirstOrDefault(x => x.Name == positionName);
+            if (position == null)
+            {
+                position = new Position
+                {
+                    Name = positionName
+                };
+                context.Positions.Add(position);
+                context.SaveChanges();
+            }
+
+            return position;
         }
 
         private static bool IsEmployeeUnique(EmployeeDto employeeDto, EmployeeDto[] deserializedEmployees)
